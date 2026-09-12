87ad9d98e54aee00b8d92d1767549f34a99f2d12
maihaicuong <111512426+maihaicuong@users.noreply.github.com>
2026-09-12 13:07:25 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Hiệu ứng chữ tự động gõ (Typing Effect)
+const typed = new Typed('.multiple-text', {
+    strings: ['Developer', 'Frontend Engineer', 'UI/UX Designer', 'Coder'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
